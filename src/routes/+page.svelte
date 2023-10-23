424e1b460977ea239434e7c168e52f096c591061
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-22 20:26:59 -0400

    feat: base facts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>the Black-Footed Ferret</h1>
+<ul>
+    <li>American polecat, prairie dog hunter, Mustela nigripes</li>
+    <li>Is a flagship species, and many other species depend on it.</li>
+    <li>Is an indicator species for the health of the grasslands and its nearby prarie dog population</li>
+    <li>They were declared extinct in 1974, rediscovered in 1981, now there is very little genetic diversity</li>
+    <li>Elizabeth Ann is a cloned black-footed ferret as a trailblazer for cloning for conservation efforts by Revive & Restore to mitigate inbreeding (and thus inbreeding depression)</li>
+</ul>
