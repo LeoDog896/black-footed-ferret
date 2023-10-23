c63715d18ab51bb39cc32bebbd497f02199054ad
Tristan F <LeoDog896@hotmail.com>
2023-10-23 08:24:03 -0400

    feat: font!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import '@fontsource/merriweather';
+	import '../app.css';
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
+<svelte:head>
+	<title>Black-Footed Ferret</title>
+</svelte:head>
+
 <h1>the Black-Footed Ferret</h1>
 <ul>
-    <li>American polecat, prairie dog hunter, Mustela nigripes</li>
-    <li>Is a flagship species, and many other species depend on it.</li>
-    <li>Is an indicator species for the health of the grasslands and its nearby prarie dog population</li>
-    <li>They were declared extinct in 1974, rediscovered in 1981, now there is very little genetic diversity</li>
-    <li>Elizabeth Ann is a cloned black-footed ferret as a trailblazer for cloning for conservation efforts by Revive & Restore to mitigate inbreeding (and thus inbreeding depression)</li>
+	<li>American polecat, prairie dog hunter, Mustela nigripes</li>
+	<li>Is a flagship species, and many other species depend on it.</li>
+	<li>
+		Is an indicator species for the health of the grasslands and its nearby prarie dog population
+	</li>
+	<li>
+		They were declared extinct in 1974, rediscovered in 1981, now there is very little genetic
+		diversity
+	</li>
+	<li>
+		Elizabeth Ann is a cloned black-footed ferret as a trailblazer for cloning for conservation
+		efforts by Revive & Restore to mitigate inbreeding (and thus inbreeding depression)
+	</li>
 </ul>
