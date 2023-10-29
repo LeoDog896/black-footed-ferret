5fc6e23111a408abbe69a968c7a808608ed0dafa
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-29 17:36:54 -0400

    feat: sources

---
+++ src/routes/sources/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const links = [
+        "https://www.worldwildlife.org/species/black-footed-ferret",
+        "https://nationalzoo.si.edu/animals/black-footed-ferret",
+        "https://www.fws.gov/species/black-footed-ferret-mustela-nigripes",
+        "https://www.nationalgeographic.com/animals/mammals/facts/black-footed-ferret",
+        "https://www.nps.gov/articles/000/black-footed-ferret-badl.htm",
+        "https://defenders.org/wildlife/black-footed-ferret"
+    ]
+</script>
+
+<h1>Sources</h1>
+
+<ul>
+    <li></li>
+</ul>
